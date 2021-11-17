8d8a964b014875cf77981f7283c0c005dc431001
DevinPate1 <dpp0005@uah.edu>
2021-11-16 22:47:33 -0600

    Updated Color scheme and layout

--- edu/uah/cs/lifeistough/NewsScreen.java
+++ edu/uah/cs/lifeistough/NewsScreen.java
@@ -10,14 +10,15 @@
 package edu.uah.cs.lifeistough;
 
 import javax.swing.JPanel;
+import javax.swing.border.*;
 
-import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.Insets;
+import java.awt.BorderLayout;
 
 
 /**
@@ -33,7 +34,8 @@ public class NewsScreen extends JPanel
     private GameManager manager;
     
     private javax.swing.JPanel newsPanel;
-    private javax.swing.JLabel newsTitle;
+    private javax.swing.JLabel breakingNews;
+    private javax.swing.JTextArea newsTitle;
     private javax.swing.JTextArea newsDescription;
     
     private javax.swing.JPanel expensePanel;
@@ -41,8 +43,10 @@ public class NewsScreen extends JPanel
     private javax.swing.JTextArea expenseReasonTextArea;
     private javax.swing.JTextArea expenseAmountTextArea;
 
+    private javax.swing.JPanel weekNumberPanel;
     private javax.swing.JLabel weekNumber;
 
+    private javax.swing.JPanel updatedPlayerBalancePanel;
     private javax.swing.JTextArea updatedPlayerBalance;
     
     private javax.swing.JButton continueButton;
@@ -51,7 +55,7 @@ public class NewsScreen extends JPanel
     private String expenseReason;
     private int expenseAmount;
     
-
+    
     /**
      * Constructs the window. Adds a reference to the game manager as well as the
      *     news event and expense information.
@@ -70,7 +74,6 @@ public class NewsScreen extends JPanel
         buildScreen();
     }
 
-    // Tells the game to pull up the stock screen when the continue button is pressed.
     private void continueButtonPressed()
     {
         manager.goToStockScreen();
@@ -79,7 +82,10 @@ public class NewsScreen extends JPanel
     
     private void buildScreen()
     {
-        //setBackground(new Color(150,0,0)); // Dark red
+        Color reddishBrown = new Color(80,0,0);
+        Color darkRed = new Color(150,0,0);
+
+        setBackground(darkRed);
 
         setLayout(new GridBagLayout());
 
@@ -91,28 +97,57 @@ public class NewsScreen extends JPanel
         int rightInset = 10;
         constraints.insets = new Insets(topInset, leftInset, bottomInset, rightInset);
         
+        Border lineBorder = new LineBorder(Color.BLACK, 5);
+        Border emptyBorder = new EmptyBorder(5,5,5,5);
+
         //*******************************************************
         // Sets the news panel
         
         newsPanel = new JPanel();
-        newsPanel.setLayout(new BorderLayout());
+        newsPanel.setLayout(new BorderLayout(0,10));
+        newsPanel.setBackground(reddishBrown);
+
+        newsPanel.setBorder(lineBorder);
+
+        // Breaking News
+        breakingNews = new javax.swing.JLabel();
+
+        breakingNews.setFont(new java.awt.Font("Tahoma", 0, 36));
+        breakingNews.setText("Breaking News: ");
+
+        breakingNews.setBackground(reddishBrown);
+        breakingNews.setForeground(Color.WHITE);
+
+        breakingNews.setBorder(new EmptyBorder(5,5,0,5));
+
+        newsPanel.add(breakingNews, BorderLayout.NORTH);
 
         // News Title
-        newsTitle = new javax.swing.JLabel();
+        newsTitle = new javax.swing.JTextArea();
         
-        newsTitle.setFont(new java.awt.Font("Tahoma", 0, 36));
-        newsTitle.setText("Breaking News: " + event.getName());
+        newsTitle.setFont(new java.awt.Font("Tahoma", 0, 28));
+        newsTitle.setText(event.getName());
+        newsTitle.setEditable(false);
 
-        newsPanel.add(newsTitle, BorderLayout.NORTH);
+        newsTitle.setBackground(reddishBrown);
+        newsTitle.setForeground(Color.WHITE);
+
+        newsTitle.setBorder(new EmptyBorder(0,5,0,5));
+
+        newsPanel.add(newsTitle, BorderLayout.CENTER);
         
         // Sets the news description
         newsDescription = new javax.swing.JTextArea();
 
         newsDescription.setFont(new java.awt.Font("Tahoma", 0, 18));
-        newsDescription.setEditable(false);
         newsDescription.setText(event.getHeadline());
+        newsDescription.setEditable(false);
 
-        newsPanel.add(newsDescription, BorderLayout.CENTER);
+        newsDescription.setBackground(reddishBrown);
+        newsDescription.setForeground(Color.WHITE);
+        newsDescription.setBorder(new EmptyBorder(0,5,5,5));
+
+        newsPanel.add(newsDescription, BorderLayout.SOUTH);
 
         constraints.gridx = 0;
         constraints.gridwidth = 4;
@@ -125,12 +160,23 @@ public class NewsScreen extends JPanel
 
         //********************************************************
         // Sets the week number
+        weekNumberPanel = new javax.swing.JPanel();
+        weekNumberPanel.setLayout(new BorderLayout());
+
+        weekNumberPanel.setBackground(reddishBrown);
+        weekNumberPanel.setBorder(lineBorder);
+
         weekNumber = new javax.swing.JLabel();
         
         weekNumber.setFont(new java.awt.Font("Tahoma", 0, 28));
         weekNumber.setText("Week " + Integer.toString(manager.getWeekNumber()));
+        weekNumber.setBackground(reddishBrown);
+        weekNumber.setForeground(Color.WHITE);
+        weekNumber.setBorder(emptyBorder);
 
-        constraints.gridx = 4;
+        weekNumberPanel.add(weekNumber, BorderLayout.CENTER);
+
+        constraints.gridx = 5;
         constraints.gridwidth = 1;
         constraints.gridy = 0;
         constraints.gridheight = 1;
@@ -138,30 +184,45 @@ public class NewsScreen extends JPanel
         constraints.fill = GridBagConstraints.NONE;
         constraints.anchor = GridBagConstraints.FIRST_LINE_END;
         
-        add(weekNumber, constraints);
+        add(weekNumberPanel, constraints);
 
         //********************************************************
         // Sets the player's bank balance after expenses
         
+        updatedPlayerBalancePanel = new javax.swing.JPanel();
+        updatedPlayerBalancePanel.setLayout(new BorderLayout());
+
+        updatedPlayerBalancePanel.setBackground(reddishBrown);
+        updatedPlayerBalancePanel.setBorder(lineBorder);
+
         updatedPlayerBalance = new javax.swing.JTextArea();
         
         updatedPlayerBalance.setFont(new java.awt.Font("Tahoma", 0, 28));
         updatedPlayerBalance.setText("Bank Balance:\n" + Integer.toString(manager.getPlayerBank()));
 
-        constraints.gridx = 4;
+        updatedPlayerBalance.setBackground(reddishBrown);
+        updatedPlayerBalance.setForeground(Color.WHITE);
+        updatedPlayerBalance.setBorder(emptyBorder);
+
+        updatedPlayerBalancePanel.add(updatedPlayerBalance, BorderLayout.CENTER);
+
+        constraints.gridx = 3;
         constraints.gridwidth = 1;
         constraints.gridy = 2;
         constraints.gridheight = 1;
 
-        constraints.anchor = GridBagConstraints.PAGE_START;
+        constraints.anchor = GridBagConstraints.FIRST_LINE_END;
 
-        add(updatedPlayerBalance, constraints);
+        add(updatedPlayerBalancePanel, constraints);
 
         //********************************************************
         // Sets Expense Panel
 
         expensePanel = new javax.swing.JPanel();
         expensePanel.setLayout(new GridBagLayout());
+
+        expensePanel.setBackground(reddishBrown);
+        expensePanel.setBorder(lineBorder);
         
         GridBagConstraints expenseConstraints = new GridBagConstraints();
         expenseConstraints.anchor = GridBagConstraints.CENTER;
@@ -171,6 +232,10 @@ public class NewsScreen extends JPanel
         
         expenseTitle.setFont(new java.awt.Font("Tahoma", 0, 28));
         expenseTitle.setText("Bills:");
+
+        expenseTitle.setBackground(reddishBrown);
+        expenseTitle.setForeground(Color.WHITE);
+        expenseTitle.setBorder(new EmptyBorder(5,5,0,5));
 
         expenseConstraints.gridx = 0;
         expenseConstraints.gridy = 0;
@@ -187,8 +252,14 @@ public class NewsScreen extends JPanel
         expenseReasonTextArea.setEditable(false);
         expenseReasonTextArea.setText(expenseReason);
 
+        expenseReasonTextArea.setBackground(reddishBrown);
+        expenseReasonTextArea.setForeground(Color.WHITE);
+        expenseReasonTextArea.setBorder(new EmptyBorder(0,5,0,5));
+
         expenseConstraints.gridx = 0;
         expenseConstraints.gridy = 1;
+
+        expenseConstraints.anchor = GridBagConstraints.LINE_START;
 
         expensePanel.add(expenseReasonTextArea, expenseConstraints);
 
@@ -198,6 +269,10 @@ public class NewsScreen extends JPanel
         expenseAmountTextArea.setFont(new java.awt.Font("Tahoma", 0, 18));
         expenseAmountTextArea.setEditable(false);
         expenseAmountTextArea.setText("$"+Integer.toString(Math.abs(expenseAmount))+" will be deducted from your bank account.");
+
+        expenseAmountTextArea.setBackground(reddishBrown);
+        expenseAmountTextArea.setForeground(Color.WHITE);
+        expenseAmountTextArea.setBorder(new EmptyBorder(0,5,5,5));
 
         expenseConstraints.gridx = 0;
         expenseConstraints.gridy = 2;
@@ -210,6 +285,7 @@ public class NewsScreen extends JPanel
         constraints.gridy = 2;
         constraints.gridheight = 1;
 
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
 
         add(expensePanel, constraints);
 
@@ -219,7 +295,7 @@ public class NewsScreen extends JPanel
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         
-        continueButton = new javax.swing.JButton("Continue to Stock Screen");
+        continueButton = new javax.swing.JButton("Go to Stock Screen");
         continueButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 continueButtonActionPerformed(evt);
@@ -230,16 +306,23 @@ public class NewsScreen extends JPanel
             }
         });
         
+        continueButton.setBorder(new BevelBorder(BevelBorder.RAISED));
+
+        buttonPanel.setBackground(reddishBrown);
+        buttonPanel.setBorder(new LineBorder(Color.BLACK, 3));
+
         buttonPanel.add(continueButton);
         
-        constraints.gridx = 4;
+        constraints.gridx = 5;
         constraints.gridy = 4;
 
-        expenseConstraints.insets = new Insets(0, 0, 0, 0);
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.LINE_END;
         
         add(buttonPanel, constraints);
         
         revalidate();
         repaint();
+        
     }
 }
