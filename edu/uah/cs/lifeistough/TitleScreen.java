51a41e6a8a67c0051b0b5bf2b4d43702c5ccd314
Luke985 <92552432+Luke985@users.noreply.github.com>
2021-11-17 09:25:54 -0600

    Update TitleScreen.java
    Refined some functions for quality of life functionality with the stock screen. Included new method restartTitle() that should be called by GameManager before updating screen when returnToTitle is called. Buffer added in the start screen helps maintain consistent spacing in StockScreen when different names are used. Key Event with the nameField prevents excessively large names.

--- edu/uah/cs/lifeistough/TitleScreen.java
+++ edu/uah/cs/lifeistough/TitleScreen.java
@@ -13,25 +13,24 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
 
 /**
  *
- * @author Luke Farris
+ * @author luked
  */
 public class TitleScreen extends JPanel{
     
-    private GameManager manager;
+    private final GameManager manager;
     
     private javax.swing.JLabel imageLabel;
     private javax.swing.JLabel TitleLabel;
@@ -55,10 +54,8 @@ public class TitleScreen extends JPanel{
     }
     
     private void openInstructions() {
-        
-        JDialog frame = new JDialog();
-        
-        JOptionPane.showMessageDialog(frame,
+                
+        JOptionPane.showMessageDialog(this,
             "To play the game, guess and press buttons \nrandomly until something"
                     + " good happens. Thanks!", "Instructions",
             JOptionPane.PLAIN_MESSAGE);
@@ -67,6 +64,23 @@ public class TitleScreen extends JPanel{
     
     private void startGame() {
         
+        nameField.setText(nameField.getText().trim());
+        
+        if(nameField.getText().length() < 1){
+            nameField.setText("         Player");
+        }
+        else if(nameField.getText().length() < 15){
+            String buffer = "";
+            for (int i = 0; i < 15 - nameField.getText().length(); i++){
+                if(i%2 == 0){
+                    buffer += "  ";
+                }
+                else{
+                    buffer +=" ";
+                }
+            }
+            nameField.setText(buffer + nameField.getText());
+        }
         manager.startGame();
         
     }
@@ -85,16 +99,18 @@ public class TitleScreen extends JPanel{
     
     public Difficulties getDifficulty() {
         
-        switch (difficultyBox.getSelectedIndex()){
-            
-            case 0:
-                return Difficulties.EASY;
-            case 1:
-                return Difficulties.NORMAL;
-            default:
-                return Difficulties.HARD;
-            
-        }
+        return switch (difficultyBox.getSelectedIndex()) {
+            case 0 -> Difficulties.EASY;
+            case 1 -> Difficulties.MEDIUM;
+            default -> Difficulties.HARD;
+        };
+        
+    }
+    
+    public void restartTitle(){
+     
+        while(nameField.getText().charAt(0) == ' ')
+            nameField.setText(nameField.getText().substring(1));
         
     }
     
@@ -114,7 +130,7 @@ public class TitleScreen extends JPanel{
         
         BufferedImage img = null;
         try {
-            img = ImageIO.read(new File("edu\\uah\\cs\\lifeistough\\logo.png"));
+            img = ImageIO.read(new File("Picture for picture.png"));
         } catch (IOException e) {
             
         }       
@@ -158,7 +174,15 @@ public class TitleScreen extends JPanel{
         namePanel.add(nameLabel);
         
         nameField = new javax.swing.JTextField();
-        nameField.setColumns(10);
+        nameField.setColumns(9);
+        nameField.setTransferHandler(null);
+        nameField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (nameField.getText().length() >= 15) // limit to between 1 and 15 characters
+                e.consume();
+            }
+        });
         nameField.setText("Player");
         
         namePanel.add(nameField);
@@ -181,39 +205,28 @@ public class TitleScreen extends JPanel{
         
         quitButton = new javax.swing.JButton("Quit");
         quitButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                QuitButtonActionPerformed(evt);
-            }
-
-            private void QuitButtonActionPerformed(ActionEvent evt) {
-                closeGame();
-            }
+                closeGame();            }
         });
         
         buttonPanel.add(quitButton);
         
         instructionsButton = new javax.swing.JButton("Instructions");
         instructionsButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                InstructionsButtonActionPerformed(evt);
-            }
-
-            private void InstructionsButtonActionPerformed(ActionEvent evt) {
                 openInstructions();
             }
-
-            
+                      
         });
         
         buttonPanel.add(instructionsButton);
         
         startButton = new javax.swing.JButton("Start");
         startButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                StartButtonActionPerformed(evt);
-            }
-
-            private void StartButtonActionPerformed(ActionEvent evt) {
                 startGame();
             }
         });
